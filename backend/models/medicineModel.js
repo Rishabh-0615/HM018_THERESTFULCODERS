43521e7ab9e4700ea9847e0609ae70ba1148b713
Pratik Chavan <pratikpchavan75@gmail.com>
2026-01-17 14:40:46 +0530

    model

--- backend/models/medicineModel.js
+++ backend/models/medicineModel.js
@@ -8,6 +8,7 @@ const medicineSchema = new mongoose.Schema(
     }],
     category: String,
     description: String,
+    image: { id: String, url: String },
 
     price: { type: Number, required: true },
     stock: { type: Number, required: true },
