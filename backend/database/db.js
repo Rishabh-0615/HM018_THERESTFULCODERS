a719e480194967c083a80511d1bbcb2e2d5c674b
Pratik Chavan <pratikpchavan75@gmail.com>
2026-01-17 12:15:16 +0530

    changes

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL,{
-            dbName : "NewDb",
+            dbName : "PCCOE",
         });
 
         console.log("connected")
